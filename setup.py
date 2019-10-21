2fcb1ef537c33133114b41db796770ba129dc8db
quyencao <boy20195@gmail.com>
2019-10-21 17:25:00 +0700

    new commit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='aih_generate',
+setup(name='aih_codegen',
       version='0.4',
       description='Generate graphql code',
       url='https://github.com/quyencao/aih_generate',
       author='aih',
       author_email='flyingcircus@example.com',
       license='MIT',
-      packages=['aih_generate'],
+      packages=['aih_codegen'],
       install_requires=[
           'JinJa2',
           'six',
